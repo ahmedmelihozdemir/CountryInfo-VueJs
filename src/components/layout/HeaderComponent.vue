0364fb5885734d15f7c3e5209da422f5ec24faea
Melih Özdemir <ozdemirmelihahmed@gmail.com>
2022-10-11 20:38:19 +0300

    add: layout components

---
+++ src/components/layout/HeaderComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="flex justify-center items-center p-2">
+        <h1>COUNTRY INFORMATION</h1>
+        <div class="flex m-4">
+            <ion-icon name="contrast-outline" class="ion-icon"></ion-icon>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.ion-icon {
+    font-size: 3rem;
+    color: white;
+    &:hover {
+        cursor: pointer;
+    }
+}
+</style>
