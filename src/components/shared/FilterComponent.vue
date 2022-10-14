6a1860444b2347483edb43dd1c528dccd6903b03
Melih Özdemir <ozdemirmelihahmed@gmail.com>
2022-10-14 20:29:57 +0300

    add: filter component and loader page.

---
+++ src/components/shared/FilterComponent.vue
@@ -0,0 +1,84 @@
+<template>
+    <div
+        class="grid xl:grid-cols-1 lg:grid-cols-1 md:grid-cols-1 sm:grid-cols-1"
+    >
+        <div class="flex justify-around items-center">
+            <label for="search" class="rounded-lg">
+                <input
+                    id="search"
+                    type="text"
+                    name="search"
+                    placeholder="Search country!"
+                    v-model="search"
+                    class="input-section border-red-400 rounded-lg p-2 m-2 bg-cyan-800 focus:border-none focus:outline-none"
+                    :class="{
+                        'bg-slate-300': lightMode,
+                    }"
+                />
+            </label>
+            <select
+                name="continent"
+                id="continent"
+                v-model="filterContinent"
+                class="select-section bg-cyan-800 rounded-lg focus:border-none focus:outline-none p-2 mx-6"
+                :class="{
+                    'bg-slate-300': lightMode,
+                }"
+            >
+                <option value="All" selected>All</option>
+                <option
+                    v-for="(continent, idx) in continents"
+                    :key="idx"
+                    class="border-2 border-red-400"
+                >
+                    {{ continent.name }}
+                </option>
+            </select>
+        </div>
+        <br />
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, reactive, provide } from "vue";
+import { useStore } from "vuex";
+
+const store = useStore();
+
+const lightMode = ref(store.state.lightMode);
+
+const continents = reactive([
+    { name: "Americas", value: "americas" },
+    { name: "Europe", value: "europa" },
+    { name: "Asia", value: "asia" },
+    { name: "Africa", value: "africa" },
+    { name: "Oceania", value: "oceania" },
+    { name: "Antarctic", value: "antarctic" },
+
+]);
+
+const filterContinent = ref("All");
+provide('filterContinent', filterContinent.value); 
+
+const search = ref();
+/* const filteredCountries = computed(()=> {
+    countries.value.filter((country)=>{
+        return country.common.name.toLowerCase().includes(search.value.toLowerCase())
+    })
+})  */
+
+
+</script>
+
+<style lang="scss" scoped>
+@media screen and (min-width: 768px) {
+    .medium {
+        width: 300px;
+    }
+}
+
+.input-section,
+.select-section {
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+}
+</style>
